5f5c0ee1ba224c8640f887cdda8547c6a84ba0df
Tallal Mansoor <tallaldev786@gmail.com>
2026-05-15 01:45:17 +0500

    commit

--- payload/src/app/(payload)/layout.tsx
+++ payload/src/app/(payload)/layout.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { RootLayout } from '@payloadcms/next/layouts'
+import config from '@payload-config'
+import { importMap } from './admin/importMap'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children
+type Args = {
+  children: React.ReactNode
 }
+
+const Layout: React.FC<Args> = async ({ children }) => {
+  return (
+    <RootLayout config={config} importMap={importMap}>
+      {children}
+    </RootLayout>
+  )
+}
+
+export default Layout
